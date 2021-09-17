9eaacd33ea295d53833844879a78a8d52d1c0189
thomasrosen <thomas.rosen@me.com>
2021-09-17 18:47:20 +0200

    whitespace

--- src/FrameChooser.js
+++ src/FrameChooser.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react'
 
-function FrameChooser({onChange}) {
+function FrameChooser({ onChange }) {
     const [frames, setFrames] = useState([])
     const [choosenFrame, setChoosenFrame] = useState(null)
 
